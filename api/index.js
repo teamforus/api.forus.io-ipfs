ccf36ef2e3bf6fad03bc788cf8639f074451300c
Jasper Tamminga <jasper.tamminga@qnh.nl>
2018-05-24 16:25:32 +0200

    Added basic querying and sorting

--- api/index.js
+++ api/index.js
@@ -39,6 +39,64 @@ router.route('/:resource/:id')
 
 
 router.route('/:resource')
+    .get(async (req, res) => {
+        var result = [];
+        
+        var query = {};
+        if (req.query.filter) {
+            query = JSON.parse(req.query.filter);
+        }
+        result = req.orbitDB.query((doc) => {
+            for (var key in query) {
+                if (doc[key] != query[key]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        var sortby = null;
+        if (req.query.sortby) {
+            sortby = req.query.sortby;
+        }
+        
+        var sortdir = "asc";
+        if (req.query.sortdir && req.query.sortdir.toLowerCase() == "desc") {
+            sortdir = "desc";
+        }
+
+        if (sortby) {
+            result.sort((a, b) => {
+                var result = 0;
+
+                if(a[sortby] && b[sortby]) {
+                    if (a[sortby] > b[sortby]) {
+                        result = 1;
+                    } else if (a[sortby] < b[sortby]) {
+                        result = -1;
+                    }
+                } else {
+                    if(a[sortby]) {
+                        result = 1;
+                    } else {
+                        result = -1;
+                    }
+                }
+
+                if (sortdir == "desc") {
+                    result = -result;
+                }
+
+                return result;
+            });
+        }
+        
+        const skip = req.query.skip ? parseInt(req.query.skip) : 0;
+        const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+        result = result.slice(skip, limit+1);
+        
+        res.json(result);
+    })
     .post(async (req, res) => {
         const item = req.body;
         if (!item.id) {
